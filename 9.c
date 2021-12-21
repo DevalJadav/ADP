90a12aa2a42016e6d665f93993e8cbea59b68cb5
DevalJadav <deval1405@gmail.com>
2021-12-21 18:58:15 +0530

    1

---
+++ 9.c
@@ -0,0 +1,22 @@
+/*
+Write a c program to print the first 10 odd numbers 
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    /* code */
+    int i=1, n=0;
+    while (n < 10)
+    {
+        if (i%2 != 0)
+        {
+            printf("%d is odd\n", i);
+            n++;
+        }
+        i++;
+    }
+    
+    return 0;
+}
