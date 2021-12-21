54cba5bd8401542f798f3dd0f3aea74c1a970c82
Deval <deval1405@gmail.com>
2021-12-21 21:30:54 +0530

    1

---
+++ 11.c
@@ -0,0 +1,18 @@
+/* Write a C program to print the even numbers b\w 1 to 100. */
+#include<stdio.h>
+
+int main()
+{
+    int i,n;
+    for (i=1; i<=100; i++)
+    {
+        if (i%2 == 0)
+        {
+            printf("%d is Even\n", i);
+        }
+        
+    }
+    
+    return 0;
+
+}
--- 7.c
+++ 7.c
@@ -8,10 +8,10 @@ int main()
    scanf("%d",&n);
  
    // Modulus (%) returns remainder
-   if (n%2 == 1)
-      printf("%d is an odd number", n);
-   else
+   if (n%2 == 0)
       printf("%d is an even number", n);
+   else
+      printf("%d is an odd number", n);
 
    return 0;
 }
