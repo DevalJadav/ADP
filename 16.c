b1fd85ee7e1053f63e3e1b2bcdd7e3c657e0d2cd
DevalJadav <deval1405@gmail.com>
2021-12-22 19:35:18 +0530

    1

--- 16.c
+++ 16.c
@@ -1,4 +1,4 @@
-//Write a C program to print the addition of 3 numbers. Accept the numbers from the user.
+//Write a C program to print the subtraction of 5 numbers. Accept the numbers from the user.
 #include<stdio.h>
 
 int main()
@@ -6,7 +6,7 @@ int main()
     int a, b ,c;
     printf("Enter your number: \n");
     scanf("%d %d %d", &a, &b, &c);
-    printf("Sum Of Three Number %d + %d + %d = %d", a, b, c, a+b+c);
+    printf("Sum Of Three Number %d - %d - %d = %d", a, b, c, a-b-c);
 
     //printf("Value: %d %d %d", a, b, c);
     return 0;
