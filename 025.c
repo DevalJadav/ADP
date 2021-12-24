8e9b2d05dff2eee939906083dd33bb50efdbfa27
DevalJadav <deval1405@gmail.com>
2021-12-24 11:00:56 +0530

    1

---
+++ 025.c
@@ -0,0 +1,25 @@
+// Write a C program to display first n odd numbers and also print their sum.
+#include <stdio.h>
+
+int main()
+{
+    int n, a = 0, b = 0, i = 1;
+    printf("Enter Your Number: \n");
+    scanf("%d", &n);
+    printf("First %d Odd Numbers are:- ", n);
+    while (a < n)
+    {
+        /* code */
+        if (i%2 != 0)
+        {
+            /* code */
+            printf("%d ", i);
+            a++;
+            b = b + i;
+        }
+        i++;
+        
+    }
+    printf("\nAnd their sum is %d\n", b);
+    return 0;
+}
