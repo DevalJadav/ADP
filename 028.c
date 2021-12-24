0d47b0a885c3dbfad72ef7a13d1103c426ca2f3c
DevalJadav <deval1405@gmail.com>
2021-12-24 14:16:36 +0530

    1

---
+++ 028.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c, i, n;
+    printf("Enter Your Number: \n");
+    scanf("%d", &n);
+    printf("Table of: %d\n",n);
+    for (i = 1; i <= 10; i++)
+    {
+        /* code */
+        b = n * i;
+        printf("%d x %d = %d\n", n, i, b);
+    }  
+}
